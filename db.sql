f55f1c937aded697c796e49d762e29f65d6c278e
Bill Edwards <wae20@cantab.net>
2015-03-31 22:06:39 +0100

    Added users and users_inventories tables.

--- db.sql
+++ db.sql
@@ -2,6 +2,12 @@ create database qmNew;
 
 	use qmNew;
 
+	create table users(
+		id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		username varchar(128) NOT NULL,
+		password varchar(128) NOT NULL
+		)ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_bin;
+
 	create table inventories(
 		id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 		name varchar(128) NOT NULL
@@ -12,6 +18,12 @@ create database qmNew;
 		name varchar(128) NOT NULL, 
 		status int(2) NOT NULL, 
 		issue varchar(128)
+		)ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_bin;
+
+	create table users_inventories(
+		id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		userId int(11) NOT NULL,
+		invId int(11) NOT NULL
 		)ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_bin;
 
 	create table inventories_items(
