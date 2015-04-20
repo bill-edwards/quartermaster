a6ad0a42d0c008fdeb433cea1ce79a09515cda97
Bill Edwards <wae20@cantab.net>
2015-04-20 16:37:18 +0100

    (FE) Added gatekeeper show/hide to titlebar.

--- public/js/directives/titlebar.js
+++ public/js/directives/titlebar.js
@@ -7,4 +7,14 @@ angular.module('titlebar',[])
 			restrict: 'E',
 			templateUrl: 'templates/panes/titlebar.html'
 		};
-	});
+	})
+
+	.controller('TitlebarController', ['$scope', 'authSyncService', function($scope, authSyncService){
+		
+		$scope.loggedIn = false; 
+
+		$scope.$on('initialise',function(){
+        	$scope.loggedIn = authSyncService.isLoggedIn();
+    	});
+
+	}]);
