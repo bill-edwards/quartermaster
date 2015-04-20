26ed3b0ff47b8adfeb8e37ed0510c3bb5f7c0f6e
Bill Edwards <wae20@cantab.net>
2015-04-20 17:41:04 +0100

    (FE) Wired up front-end to use /api/logout.

--- public/js/directives/titlebar.js
+++ public/js/directives/titlebar.js
@@ -9,9 +9,10 @@ angular.module('titlebar',[])
 		};
 	})
 
-	// Control visibility of titlebar according to log-in state. 
-	.controller('TitlebarController', ['$scope', 'authSyncService', function($scope, authSyncService){
+	.controller('TitlebarController', ['$scope','$http','$location','authSyncService', function($scope, $http, $location, authSyncService){
 		
+		// Control visibility of titlebar according to log-in state. 
+
 		$scope.loggedIn = false; 
 
 		$scope.$on('initialise',function(){
@@ -21,5 +22,25 @@ angular.module('titlebar',[])
     	$scope.$on('login',function(){
         	$scope.loggedIn = true;
     	});
+
+    	$scope.$on('logout',function(){
+        	$scope.loggedIn = false;
+    	});
+
+
+    	// Define behaviour of log-out button. 
+
+    	$scope.logout = function(){
+    		$http.post('/api/logout')
+			.success(function(data){ 
+				// Broadcast logout event to tell titlebar to become hidden. 
+				$scope.$broadcast('logout');
+				// Re-direct to welcome page.
+				$location.path('/welcome');
+			})
+			.error(function(err){
+				console.log("QMErr: Log-out could not be completed, please try again");
+			});
+    	};
 
 	}]);
