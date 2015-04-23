e7e2fe5a5e9e8b2506855a53a825602316c903b2
Bill Edwards <wae20@cantab.net>
2015-04-23 21:47:16 +0100

    (FE) Added error reporting to view_inventory page.

--- public/js/controllers/view_inventory.js
+++ public/js/controllers/view_inventory.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('viewInventory',[])
-	.controller('ViewInvController', ['$scope', '$routeParams', '$http', '$location','authSyncService', function($scope, $routeParams, $http, $location, authSyncService){
+	.controller('ViewInvController', ['$scope', '$routeParams', '$http', '$location','$rootScope','authSyncService', function($scope, $routeParams, $http, $location, $rootScope, authSyncService){
 		
 		// Gatekeeper
 		$scope.$on('initialise', function(){
@@ -16,12 +16,20 @@ angular.module('viewInventory',[])
 		.success(function(data){
 			$scope.inventory = data; 
 		})
-		.error(function(err){
-			console.log("QMErr: Data could not be retrieved from server");
-		});
-		
-		$scope.$on('initialise',function(){
-			if (!authSyncService.isLoggedIn()) $location.path('/welcome');
+		.error(function(err, status){
+			if (status==401){
+				window.alert('You seem to have been logged-out. Please log-in to continue.');
+				// Broadcast logout event to tell titlebar to become hidden. 
+				$rootScope.$broadcast('logout');
+				// Re-direct to welcome page.
+				$location.path('/welcome');
+			}
+			else if (status==404){
+				window.alert('We can\'t find any inventory of yours with this id');
+				// Re-direct to welcome page.
+				$location.path('/home');
+			}
+			else console.log('unknown error');
 		});
 		
 	}]);
