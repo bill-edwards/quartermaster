85f85f983ba31859fc1eb86e1a60863af1e74f3f
Bill Edwards <wae20@cantab.net>
2015-06-08 19:39:17 +0100

    (FE) On edit-inventory page added possibility of searching for and adding items from other inventories.

--- public/js/controllers/edit_inventory.js
+++ public/js/controllers/edit_inventory.js
@@ -22,7 +22,6 @@ angular.module('editInventory',[])
 				item.inOut='in';
 			});
 			$scope.invNameField = $scope.inventory.name; // Must be named separately so we can detemine if it has been changed when saving changes. 
-			console.log(data);
 		})
 		.error(function(err, status){
 			if (status==401){
@@ -132,7 +131,6 @@ angular.module('editInventory',[])
 				// Submit data to server. 
 				$http.post('api/item/new', {name:$scope.name, invId:$scope.inventory.id})
 				.success(function(data){
-					console.log(data);
 					var newItem = {id:data.id, name:$scope.name, inOut:"in", editStatus:"O"};
 					$scope.inventory.items.push(newItem);
 					$scope.name = "";
@@ -154,5 +152,76 @@ angular.module('editInventory',[])
 					else console.log("unknown error");
 				});
 			}
+		};
+	}])
+
+	.controller('SearchByInventoryController', ['$http', '$scope', function($http, $scope){
+			
+		// Query back-end for details of user's inventories. 
+		$http.get('api/user/me')
+		.success(function(data){
+			// Remove the currently edited inventory from the drop-down list. 
+			$scope.inventories = data.inventories.filter(function(inventory){
+				return inventory.id != $scope.inventory.id; 
+			}); 
+		})
+		.error(function(err){
+			console.log('error with getting user details');
+		});
+
+		// Initially have no inventory selected. 
+		$scope.otherInventoryId = 0;
+
+		// Function called when inventory is selected from drop-down menu. 
+		$scope.getItems = function(){
+			// If an inventory is selected: 
+			if($scope.otherInventoryId!=0) {
+				$http.get('api/inventory/'+$scope.otherInventoryId)
+				.success(function(data){
+					$scope.otherInventory = data; 
+				})
+				.error(function(err){
+					console.log('error with retrieving other inventory');
+				});
+			}
+			else $scope.otherInventory = [];
+		};
+
+		// Function called when 'add all items' button is pressed. 
+		$scope.addAllItems = function(){
+			// Deep copy each item, add extra fields, push to array of items in main page scope. 
+			$scope.otherInventory.items.forEach(function(item){
+				var addedItem = {};
+				for (var prop in item){
+					addedItem[prop] = item[prop]; 
+				}
+				addedItem.inOut = 'in';
+				addedItem.editStatus = 'A';
+				$scope.inventory.items.push(addedItem); 
+			});
+			// Reset choice of inventory. 
+			$scope.otherInventoryId = 0; 
+			$scope.getItems(); 
+		};
+
+	}])
+
+	.controller('OtherInvItemController', ['$scope', function($scope){
+
+		$scope.addItem = function(){
+
+			// Deep copy item, and add extra fields. 
+			var addedItem = {};
+			for (var prop in $scope.item){
+				addedItem[prop] = $scope.item[prop]; 
+			}
+			addedItem.inOut = 'in';
+			addedItem.editStatus = 'A';
+			// Push item into array of items on main page scope. 
+			$scope.inventory.items.push(addedItem); 
+			// Remove item from array of items on search-by-inventory scope. 
+			$scope.otherInventory.items = $scope.otherInventory.items.filter(function(item){
+				return item.id != $scope.item.id; 
+			});
 		};
 	}]);
