404beb3a0792df6d7e4b3305e55df6581a39709d
Bill Edwards <wae20@cantab.net>
2015-06-16 11:38:25 +0100

    (FE) Implemented delete-inventory at frontend.

--- public/js/controllers/view_inventory.js
+++ public/js/controllers/view_inventory.js
@@ -7,6 +7,7 @@ angular.module('viewInventory',[])
 		authSyncService.authStatus(function(){
 
 			var invId = $routeParams.invId; 
+			$scope.errors = {form:""};
 
 			$http.get('api/inventory/' + invId)
 			.success(function(data){
@@ -28,5 +29,32 @@ angular.module('viewInventory',[])
 				}
 				else console.log('unknown error');
 			});
+
+			// Function called when pressing delete button. 
+			$scope.deleteInventory = function(){
+				if (window.confirm('Are you sure you want to delete this inventory?')){
+					$http.delete('api/inventory/' + invId)
+					.success(function(){
+						$location.path('/home');
+					})
+					.error(function(err, status){
+						if (status==401){
+							window.alert('You seem to have been logged-out. Please log-in to continue.');
+							// Broadcast logout event to tell titlebar to become hidden. 
+							$rootScope.$broadcast('logout');
+							// Re-direct to welcome page.
+							$location.path('/welcome');
+						}
+						else if (status==404){
+							$scope.errors.form = "We're having trouble locating this item in our records. Please refresh this page and try again.";
+						}
+						else if (status==500){
+							$scope.errors.form = "Sorry, there was a problem connecting to the server. Please try again.";
+						}
+						else console.log("QMErr: Data could not be retrieved from server");
+					});
+				}
+			};
+
 		});
 	}]);
