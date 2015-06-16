04ed1856c32125db4a2fe6445fe1936f5a6ea8ac
Bill Edwards <wae20@cantab.net>
2015-06-16 12:12:08 +0100

    (FE) Front-end handling for delete-event; improved display of date/countdown on view-event page.

--- public/js/controllers/view_event.js
+++ public/js/controllers/view_event.js
@@ -7,6 +7,7 @@ angular.module('viewEvent',[])
 		authSyncService.authStatus(function(){
 
 			var eventId = $routeParams.eventId; 
+			$scope.errors = {form:""};
 
 			$http.get('api/event/' + eventId)
 			.success(function(data){
@@ -32,5 +33,31 @@ angular.module('viewEvent',[])
 				}
 				else console.log('unknown error');
 			});
+
+			// Function called when pressing delete button. 
+			$scope.deleteEvent = function(){
+				if (window.confirm('Are you sure you want to delete this event?')){
+					$http.delete('api/event/' + eventId)
+					.success(function(){
+						$location.path('/home');
+					})
+					.error(function(err, status){
+						if (status==401){
+							window.alert('You seem to have been logged-out. Please log-in to continue.');
+							// Broadcast logout event to tell titlebar to become hidden. 
+							$rootScope.$broadcast('logout');
+							// Re-direct to welcome page.
+							$location.path('/welcome');
+						}
+						else if (status==404){
+							$scope.errors.form = "We're having trouble locating this event in our records. Please refresh this page and try again.";
+						}
+						else if (status==500){
+							$scope.errors.form = "Sorry, there was a problem connecting to the server. Please try again.";
+						}
+						else console.log("QMErr: Data could not be retrieved from server");
+					});
+				}
+			};
 		});
 	}]);
